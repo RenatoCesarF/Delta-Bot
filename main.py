0874d6edf64edc373da47338bc83f4d69b236057
Renato Cesar <re.fbarcellos@hotmail.com>
2021-04-03 22:26:26 -0300

    :tada: Starting the project!

---
+++ main.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import discord
+
+import utils.enviroment as enviroment
+import utils.client 
+
+env =  enviroment.EnvVars()
+print(env.APLICATION_ID)
+#client = client.MyClient()
+#client.run('ODI4MDc4MTM4Mjc3ODg4MDAw.YGkV3Q.q6sIqbqU3yvBEr8MP49u6AWvsmA')
---
+++ utils/enviroment.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+from os.path import join, dirname
+import os
+
+class EnvVars():
+    def __init__(self):
+        self.APLICATION_ID = ''
+        self.PUBLIC_KEY = ''
+        self.TOKEN = ''
+
+        dotenv_path = join(dirname(__file__), '.env')
+        load_dotenv(dotenv_path)
+
+        self.APLICATION_ID = os.environ.get("APLICATION_ID")
+        self.PUBLIC_KEY = os.environ.get("PUBLIC_KEY")
+        self.TOKEN = os.environ.get("TOKEN")
+      

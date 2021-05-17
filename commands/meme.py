733d217cac374cb2a99ddd937bb02e8bd109898e
Renato Cesar <re.fbarcellos@hotmail.com>
2021-05-17 09:53:42 -0400

    :sparkles: First version of meme command

--- commands/meme.py
+++ commands/meme.py
@@ -1,8 +1,6 @@
 import discord
 from discord.ext import commands
-
-templates = [ 'mini-keanu','sparta','buzz']
-
+from utils.memeTemplates import templates
 
 
 class MemeCommand(commands.Cog):
@@ -12,12 +10,12 @@ class MemeCommand(commands.Cog):
     
     @commands.command(name="meme")
     async def _meme(self, ctx, template = '', *, params = ''):
-        
         if template == '' or template == 'help':
             await ctx.send("You need to Add a **template** followed by some **params**, write `>help meme` to see some templates")
 
-        if template in templates:
-            link = f"https://api.memegen.link/images/{template}/{'/'.join(param.replace(' ', '_') for param in params.split(','))}.png"
+        if next(item for item in templates if item["name"] == template) in templates:
+            templateLink = next(item for item in templates if item["name"] == template)['link']
+            link = f"https://api.memegen.link/images/{templateLink}/{'/'.join(param.replace(' ', '_') for param in params.split(','))}.png"
             await ctx.send(link)
           
 
